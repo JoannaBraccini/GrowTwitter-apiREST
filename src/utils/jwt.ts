43dcf38ff87fb6beaea419e34952adbf9e802f82
Joanna <joannabraccini@gmail.com>
2025-01-08 18:30:46 -0300

    config verify()

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,6 +5,7 @@ import { error } from "console";
 export class JWT {
   public generateToken(data: AuthUser): string {
     if (!process.env.JWT_SECRET) throw new Error("Secret not defined");
+
     const token = jwt.sign(data, process.env.JWT_SECRET, {
       algorithm: "HS256",
       expiresIn: process.env.JWT_EXPIRES_IN,
@@ -12,7 +13,14 @@ export class JWT {
 
     return token;
   }
-  public verifyToken() {
-    jwt.verify();
+  public verifyToken(token: string): AuthUser | null {
+    try {
+      if (!process.env.JWT_SECRET) throw new Error("Secret not defined");
+
+      const data = jwt.verify(token, process.env.JWT_SECRET) as AuthUser;
+      return data;
+    } catch {
+      return null;
+    }
   }
 }
